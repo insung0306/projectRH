21367422cdbcd36fef465f56ea36419604284811
insung <jis020306@gmail.com>
2019-06-09 21:59:12 +0900

    fix

--- server_test.js
+++ server_test.js
@@ -3,11 +3,8 @@ var rh_fs = require('fs');
 
 var rh_server = rh_http.createServer(function(req,res){
     rh_fs.readFile('test.html',function(err, data){
-        res.writeHead(200, {'Content-Type' : 'test/html'});
+        //res.writeHead(200, {'Content-Type' : 'test/html'});
         res.write(data);
         res.end();    
     });
 }).listen(3000);
-
-
-

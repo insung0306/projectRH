b6b1cd4cc41f4ba080080f61f705ac7c89e1b0b1
insung <jis020306@gmail.com>
2019-06-12 14:28:44 +0900

    add

--- server_test.js
+++ server_test.js
@@ -1,22 +1,30 @@
 var rh_http = require('http');
 var rh_fs = require('fs');
+var rh_url = require('url');
 
 var rh_server = rh_http.createServer(function(req,res){
     //rh_fs.readFile('test.html',function(err, data){
         //res.writeHead(200, {'Content-Type': 'test/html'});
         //res.write(data);
         //res.end();
+    var uri = req.url;
+    var query = rh_url.parse(uri,true).query;
     if(req.url == "/"){
-        res.writeHead(200,{'Content-Type':'text/html'});
-        res.write('<html><body><p>This is home Page.</p></body></html>');
-        res.end();
-    }else if(req.url == "/admin"){
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write('<html><body><p>This is admin Page.</p></body></html>');
-        res.end();
-    }else if(req.url == "/uproadFile"){
-        //rh_fs.readFile()
-        
+        rh_fs.readFile('test.html', function(error,data){
+            res.writeHead(200, {'Content-Type' : 'text/html'})
+            res.write(data);
+            res.end();
+        })
     }
-    //});
+    else if(req.url == "/uploadfile"){
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.write('<html><body><p>Upload</p></body></html>');
+        res.end();
+    }
+    else{
+        res.write('<html><body><p>404 not found</p></body></html>');
+        res.end();
+    }
+    
 }).listen(3000);
+
